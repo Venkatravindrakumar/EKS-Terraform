ec76c90a441e1c94f8d72cbb9839614fd3465c6e
Neti Venkatravindrakumar <149747814+Venkatravindrakumar@users.noreply.github.com>
2024-12-16 12:45:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,55 +2,55 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_vpc" "devopsshacks_vpc" {
+resource "aws_vpc" "devopsshack_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "devopsshacks-vpc"
+    Name = "devopsshack-vpc"
   }
 }
 
-resource "aws_subnet" "devopsshacks_subnet" {
+resource "aws_subnet" "devopsshack_subnet" {
   count = 2
-  vpc_id                  = aws_vpc.devopsshacks_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.devopsshacks_vpc.cidr_block, 8, count.index)
+  vpc_id                  = aws_vpc.devopsshack_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.devopsshack_vpc.cidr_block, 8, count.index)
   availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "devopsshacks-subnet-${count.index}"
+    Name = "devopsshack-subnet-${count.index}"
   }
 }
 
-resource "aws_internet_gateway" "devopsshacks_igw" {
-  vpc_id = aws_vpc.devopsshacks_vpc.id
+resource "aws_internet_gateway" "devopsshack_igw" {
+  vpc_id = aws_vpc.devopsshack_vpc.id
 
   tags = {
-    Name = "devopsshacks-igw"
+    Name = "devopsshack-igw"
   }
 }
 
-resource "aws_route_table" "devopsshacks_route_table" {
-  vpc_id = aws_vpc.devopsshacks_vpc.id
+resource "aws_route_table" "devopsshack_route_table" {
+  vpc_id = aws_vpc.devopsshack_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.devopsshacks_igw.id
+    gateway_id = aws_internet_gateway.devopsshack_igw.id
   }
 
   tags = {
-    Name = "devopsshacks-route-table"
+    Name = "devopsshack-route-table"
   }
 }
 
 resource "aws_route_table_association" "a" {
   count          = 2
-  subnet_id      = aws_subnet.devopsshacks_subnet[count.index].id
-  route_table_id = aws_route_table.devopsshacks_route_table.id
+  subnet_id      = aws_subnet.devopsshack_subnet[count.index].id
+  route_table_id = aws_route_table.devopsshack_route_table.id
 }
 
-resource "aws_security_group" "devopsshacks_cluster_sg" {
-  vpc_id = aws_vpc.devopsshacks_vpc.id
+resource "aws_security_group" "devopsshack_cluster_sg" {
+  vpc_id = aws_vpc.devopsshack_vpc.id
 
   egress {
     from_port   = 0
@@ -60,12 +60,12 @@ resource "aws_security_group" "devopsshacks_cluster_sg" {
   }
 
   tags = {
-    Name = "devopsshacks-cluster-sg"
+    Name = "devopsshack-cluster-sg"
   }
 }
 
-resource "aws_security_group" "devopsshacks_node_sg" {
-  vpc_id = aws_vpc.devopsshacks_vpc.id
+resource "aws_security_group" "devopsshack_node_sg" {
+  vpc_id = aws_vpc.devopsshack_vpc.id
 
   ingress {
     from_port   = 0
@@ -82,25 +82,25 @@ resource "aws_security_group" "devopsshacks_node_sg" {
   }
 
   tags = {
-    Name = "devopsshacks-node-sg"
+    Name = "devopsshack-node-sg"
   }
 }
 
-resource "aws_eks_cluster" "devopsshacks" {
-  name     = "devopsshacks-cluster"
-  role_arn = aws_iam_role.devopsshacks_cluster_role.arn
+resource "aws_eks_cluster" "devopsshack" {
+  name     = "devopsshack-cluster"
+  role_arn = aws_iam_role.devopsshack_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = aws_subnet.devopsshacks_subnet[*].id
-    security_group_ids = [aws_security_group.devopsshacks_cluster_sg.id]
+    subnet_ids         = aws_subnet.devopsshack_subnet[*].id
+    security_group_ids = [aws_security_group.devopsshack_cluster_sg.id]
   }
 }
 
 resource "aws_eks_node_group" "devopsshacks" {
-  cluster_name    = aws_eks_cluster.devopsshacks.name
-  node_group_name = "devopsshacks-node-group"
-  node_role_arn   = aws_iam_role.devopsshacks_node_group_role.arn
-  subnet_ids      = aws_subnet.devopsshacks_subnet[*].id
+  cluster_name    = aws_eks_cluster.devopsshack.name
+  node_group_name = "devopsshack-node-group"
+  node_role_arn   = aws_iam_role.devopsshack_node_group_role.arn
+  subnet_ids      = aws_subnet.devopsshack_subnet[*].id
 
   scaling_config {
     desired_size = 3
@@ -112,11 +112,11 @@ resource "aws_eks_node_group" "devopsshacks" {
 
   remote_access {
     ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.devopsshacks_node_sg.id]
+    source_security_group_ids = [aws_security_group.devopsshack_node_sg.id]
   }
 }
 
-resource "aws_iam_role" "devopsshacks_cluster_role" {
+resource "aws_iam_role" "devopsshack_cluster_role" {
   name = "devopsshacks-cluster-role"
 
   assume_role_policy = <<EOF
@@ -135,13 +135,13 @@ resource "aws_iam_role" "devopsshacks_cluster_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshacks_cluster_role_policy" {
-  role       = aws_iam_role.devopsshacks_cluster_role.name
+resource "aws_iam_role_policy_attachment" "devopsshack_cluster_role_policy" {
+  role       = aws_iam_role.devopsshack_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role" "devopsshacks_node_group_role" {
-  name = "devopsshacks-node-group-role"
+resource "aws_iam_role" "devopsshack_node_group_role" {
+  name = "devopsshack-node-group-role"
 
   assume_role_policy = <<EOF
 {
@@ -159,17 +159,17 @@ resource "aws_iam_role" "devopsshacks_node_group_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshacks_node_group_role_policy" {
+resource "aws_iam_role_policy_attachment" "devopsshack_node_group_role_policy" {
   role       = aws_iam_role.devopsshacks_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshacks_node_group_cni_policy" {
+resource "aws_iam_role_policy_attachment" "devopsshack_node_group_cni_policy" {
   role       = aws_iam_role.Primary_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshacks_node_group_registry_policy" {
+resource "aws_iam_role_policy_attachment" "devopsshack_node_group_registry_policy" {
   role       = aws_iam_role.devopsshacks_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
